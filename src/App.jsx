764b605b139cf173a00f561afd45b697fcc37810
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-23 00:09:53 +0600

    error page setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
       <div>
+        <h1>Hello That a new page</h1>
       </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import ErrorPage from './components/ErrorPage.jsx'
+
+let router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+    errorElement: <ErrorPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
